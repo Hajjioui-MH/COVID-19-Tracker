f4f40d1c89ec96b9dfee0785328d2f9b3c600cbf
Hajjioui-MH <hajjioui.mh@gmail.com>
2020-07-23 03:23:06 +0100

    fixing Bar chart using country data and react-chartjs-2 backage

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,24 +1,14 @@
-import React,{ useState, useEffect } from 'react';
+import React from 'react';
 import Card from './Card';
-import {fetchCardData} from '../../api' //return an object
 
-const Cards = ({ country })=>{
+const Cards = ({ data:{confirmed,deaths,recovered,lastUpdate} })=>{
 
-	const [data,setData] = useState({});
-	useEffect( ()=>{
-		const fetch_api = async ()=>{
-			const initial_Card_Data = await fetchCardData(country);
-			setData(initial_Card_Data);
-		}
-		fetch_api()
-	},[country])
-
-	if (data.confirmed) {
+	if (confirmed) {
 		return(
 			<div className='container'>
-				<Card title={'Infected'} value={data.confirmed} color={'blue'} date={new Date(data.lastUpdate).toDateString()} description={'Number of infected caused by COVID-19.'} />
-				<Card title={'Recovered'} value={data.recovered} color={'green'} date={new Date(data.lastUpdate).toDateString()} description={'Number of recoveries from COVID-19.'} />
-				<Card title={'Deaths'} value={data.deaths} color={'red'} date={new Date(data.lastUpdate).toDateString()} description={'Number of deaths caused by COVID-19.'} />
+				<Card title={'Infected'} value={confirmed} color={'blue'} date={new Date(lastUpdate).toDateString()} description={'Number of infected caused by COVID-19.'} />
+				<Card title={'Recovered'} value={recovered} color={'green'} date={new Date(lastUpdate).toDateString()} description={'Number of recoveries from COVID-19.'} />
+				<Card title={'Deaths'} value={deaths} color={'red'} date={new Date(lastUpdate).toDateString()} description={'Number of deaths caused by COVID-19.'} />
 			</div>
 		 )	
 	}else{
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,31 @@
 import React,{ Component } from 'react';
 import {Cards, CountryPicker, Chart} from '../components'
+import {fetchCardData} from '../api' //return an object
+import covidImg from '../images/coronaImg.png';
 import './App.css';
 
 class App extends Component {
   state = {
     country : '',
+    data : {} ,
   }
-  handleCountryChange = (clickedCountry)=>{
-    this.setState({ country : clickedCountry })
+
+  async componentDidMount(){
+    const initial_Card_Data = await fetchCardData();
+    this.setState({data : initial_Card_Data})
+  }
+  handleCountryChange = async (clickedCountry)=>{
+    const Country_Data = await fetchCardData(clickedCountry);
+    this.setState({ data : Country_Data, country : clickedCountry })
   }
   render(){
-    const { country } = this.state;
+    const {  data, country } = this.state;
     return (
       <div className="App">
-        <Cards country={country} />
+        <img className='coronaImg mt3' src={covidImg} alt='COVID-19' />
+        <Cards data={data} />
         <CountryPicker countryChange={this.handleCountryChange} />
-        <Chart country={country} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
